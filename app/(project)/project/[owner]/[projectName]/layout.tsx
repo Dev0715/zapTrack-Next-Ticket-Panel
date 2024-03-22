1a80847cfa22455060e440a4e7b38919cd2f256b
Dev0715 <webdev0715@gmail.com>
2024-03-22 19:15:18 +0900

    feat(team): add `team` page

---
+++ app/(project)/project/[owner]/[projectName]/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import SidebarProject from "@/app/(project)/_components/SidebarProject/page";
+
+const ProjectLayout = ({
+    children, params
+}: {
+    children: React.ReactNode;
+    params: {
+        owner: string,
+        projectName: string
+    }
+}) => {
+    return <>
+        <div className="flex h-full" style={{ minHeight: `calc(100vh - 48px)` }}>
+            <SidebarProject projectName={params.projectName} owner={params.owner} />
+            {
+                children
+            }
+        </div>
+    </>
+}
+
+export default ProjectLayout;
