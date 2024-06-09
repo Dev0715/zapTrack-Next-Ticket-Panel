f48a91f3723af93dcb66382b05d4ce5cf5055d0f
Dev0715 <webdev0715@gmail.com>
2024-06-09 23:35:30 +0900

    feat: update style

--- app/(authed)/_components/KanbanHeader/page.tsx
+++ app/(authed)/_components/KanbanHeader/page.tsx
@@ -13,29 +13,29 @@ interface KanbanHeaderProps {
 
 const KanbanHeader = ({ status, handleAddUserstory, handleAddBulk }: KanbanHeaderProps) => {
     return <>
-        <h2 className={`text-[1rem] items-center bg-[#d8dee9] border-tl-[4px] border-tr-[4px] text-[#5a5b72] flex basis-[292px] grow-0 shrink-0 h-[36px] justify-between mr-[4px] min-h-[36px] max-w-[292px] px-3 relative uppercase duration-100 vtransition-all leading-tight`}>
+        <h2 className={`text-[1rem] items-center bg-[#d8dee9] rounded-tl-[4px] rounded-tr-[4px] text-[#5a5b72] flex basis-[292px] grow-0 shrink-0 h-[36px] justify-between mr-[4px] min-h-[36px] max-w-[292px] px-3 relative uppercase duration-100 vtransition-all leading-tight`}>
             <div className={`grow shrink-0 h-[1rem] max-w-[0.625rem] w-[0.625rem] bg-[${status.color}]`} />
             <div className="flex items-end ml-2 w-full">
                 <div className="inline-block max-w-[160px] overflow-hidden text-ellipsis text-[.875rem] text-[#2e3440] leading-[1em]">{status.name}</div>
             </div>
             <div className="flex">
-                {/* <button
+                <button
                     title="Add new user story"
                     className="mr-[2px] bg-transparent text-[#70728f] p-1 text-[.875rem] items-center border-0 rounded-[4px] inline-flex justify-center text-center uppercase"
                     onClick={() => handleAddUserstory(status)}
                 >
                     <IoMdAdd />
-                </button> */}
+                </button>
                 <button
                     title="Add new bulk"
-                    className="mr-[2px] bg-transparent text-[#70728f] p-1 text-[.875rem] items-center border-0 rounded-[4px] inline-flex justify-center text-center uppercas"
+                    className="mr-[2px] bg-transparent text-[#70728f] p-1 text-[.875rem] items-center border-0 rounded-[4px] inline-flex justify-center text-center uppercase"
                     onClick={() => handleAddBulk(status)}
                 >
                     <FaShirtsinbulk />
                 </button>
                 <button
                     title="Fold column"
-                    className="mr-[2px] bg-transparent text-[#70728f] p-1 text-[.875rem] items-center border-0 rounded-[4px] inline-flex justify-center text-center uppercas"
+                    className="mr-[2px] bg-transparent text-[#70728f] p-1 text-[.875rem] items-center border-0 rounded-[4px] inline-flex justify-center text-center uppercase hidden"
                 >
                     <FaAngleLeft />
                 </button>
